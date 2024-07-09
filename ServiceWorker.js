041a3341040bf8e3a57e2798ffa18bfaede6068a
umut3RC <maeumut@gmail.com>
2024-07-09 19:59:09 +0300

    Added player pull drag drop

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Bottle Bottom Games-Retirement Rush-1.0";
+const contentToCache = [
+    "Build/retirement_rush.loader.js",
+    "Build/retirement_rush.framework.js",
+    "Build/retirement_rush.data",
+    "Build/retirement_rush.wasm",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
